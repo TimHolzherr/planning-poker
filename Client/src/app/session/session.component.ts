0b878165a95e462311dfbab5bc46ec3972272450
TimHolzherr <tim_holzherr@bluewin.ch>
2018-12-26 00:29:14 +0100

    Style session component with bulma

--- Client/src/app/session/session.component.ts
+++ Client/src/app/session/session.component.ts
@@ -7,71 +7,110 @@ import { BackendService } from "../backend.service";
 @Component({
     selector: "app-session",
     template: `
-        <div>
-            <span>
-                {{ model.numberOfUsers }} people joined the session for
-                <strong>{{ this.model.name }}</strong>
-            </span>
-            <span>
-                invite other to join at
-                <a [href]="currentUrl">{{ currentUrl }}</a></span
-            >
+        <div class="hero has-text-centered is-light">
+            <div class="title is-1">{{ this.model.name }}</div>
+            <div class="subtitle"></div>
         </div>
-
+        <div class="section">
+            <div class="notification has-text-centered is-info">
+                <p>
+                    {{ model.numberOfUsers }} member{{
+                        model.numberOfUsers > 1 ? "s" : ""
+                    }}
+                    joined the session.
+                </p>
+                <p>
+                    Invite others to join at
+                    <a [href]="currentUrl">{{ currentUrl }}</a>
+                </p>
+            </div>
+        </div>
         <div *ngIf="!model.hasTickets()">
-            <form (submit)="createNewTicket(nextTicketName)">
-                <label for="ticketName">Create the first ticket</label>
-                <input
-                    [(ngModel)]="nextTicketName"
-                    name="ticketName"
-                    id="ticketName"
-                    placeholder="Name of the Ticket"
-                    required
-                />
-                <input
-                    [disabled]="nextTicketName === ''"
-                    class="button-primary"
-                    type="submit"
-                    value="Create new Ticket"
-                />
-            </form>
+            <div class="section">
+                <form (submit)="createNewTicket(nextTicketName)">
+                    <div class="block">
+                        <p class="control field has-text-centered">
+                            <label for="ticketName" class="label"
+                                >Create the first ticket</label
+                            >
+                        </p>
+                        <div class="field has-addons has-addons-centered">
+                            <p class="control">
+                                <input
+                                    class="input"
+                                    [(ngModel)]="nextTicketName"
+                                    name="ticketName"
+                                    id="ticketName"
+                                    placeholder="Name of the Ticket"
+                                    required
+                                />
+                            </p>
+                            <p class="control">
+                                <input
+                                    [disabled]="nextTicketName === ''"
+                                    class="button is-info"
+                                    type="submit"
+                                    value="Create new Ticket"
+                                />
+                            </p>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
         <div *ngIf="model.hasTickets()">
-            <app-ticket
-                [ticket]="model.currentTicket"
-                [numberOfUsers]="this.model.numberOfUsers"
-            ></app-ticket>
+            <div class="box">
+                <app-ticket
+                    [ticket]="model.currentTicket"
+                    [numberOfUsers]="this.model.numberOfUsers"
+                ></app-ticket>
+            </div>
             <div class="footer">
                 <form (submit)="createNewTicket(nextTicketName)">
-                    <input
-                        [disabled]="model.isfirstTicket()"
-                        class="button-primary"
-                        type="button"
-                        value="Previous"
-                        (click)="model.goToPreviousTicket()"
-                    />
-                    <ng-container *ngIf="model.isLastTicket()">
-                        <input
-                            name="nextTicketName"
-                            required
-                            placeholder="Name of next ticket"
-                            [(ngModel)]="nextTicketName"
-                        />
-                        <input
-                            [disabled]="nextTicketName === ''"
-                            class="button-primary"
-                            type="submit"
-                            value="Create new Ticket"
-                        />
-                    </ng-container>
-                    <ng-container *ngIf="!model.isLastTicket()">
-                        <input
-                            class="button-primary"
-                            type="button"
-                            value="Next"
-                            (click)="model.goToNextTicket()"
-                        />
-                    </ng-container>
+                    <div class="level">
+                        <div class="level-left">
+                            <input
+                                [disabled]="model.isfirstTicket()"
+                                class="button level-item"
+                                type="button"
+                                value="Previous"
+                                (click)="model.goToPreviousTicket()"
+                            />
+                        </div>
+                        <div class="level-right">
+                            <div class="field has-addons">
+                                <ng-container *ngIf="model.isLastTicket()">
+                                    <p class="control">
+                                        <input
+                                            class="input level-item"
+                                            name="nextTicketName"
+                                            required
+                                            placeholder="Name of next ticket"
+                                            [(ngModel)]="nextTicketName"
+                                        />
+                                    </p>
+                                    <p class="control">
+                                        <input
+                                            [disabled]="nextTicketName === ''"
+                                            class="button level-item"
+                                            type="submit"
+                                            value="Create new Ticket"
+                                        />
+                                    </p>
+                                </ng-container>
+                                <ng-container *ngIf="!model.isLastTicket()">
+                                    <p class="control">
+                                        <input
+                                            class="button level-item"
+                                            type="button"
+                                            value="Next"
+                                            (click)="model.goToNextTicket()"
+                                        />
+                                    </p>
+                                </ng-container>
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
