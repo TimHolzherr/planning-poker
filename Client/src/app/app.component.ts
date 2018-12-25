b3e53a4d9aa3de11cdbea186b08186c59a1c3f57
TimHolzherr <tim_holzherr@bluewin.ch>
2018-12-25 23:45:54 +0100

    Styling with Bulma for welcome page

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: "app-root",
     template: `
-        <div style="text-align:center; width: 50%; margin: 0 auto;">
-            <router-outlet></router-outlet>
-        </div>
+        <div class="container fill-page"><router-outlet></router-outlet></div>
     `,
     styles: []
 })
--- Client/src/app/welcome/welcome.component.ts
+++ Client/src/app/welcome/welcome.component.ts
@@ -4,30 +4,87 @@ import { Router } from "@angular/router";
 @Component({
     selector: "app-welcome",
     template: `
-        <h1>Wellcome to Planning Poker</h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <form (submit)="CreateNewSession()">
-            <input
-                name="sessionName"
-                placeholder="Session Name"
-                [(ngModel)]="sessionName"
-                required
-            />
-            <input
-                [disabled]="!this.sessionName"
-                class="button-primary"
-                type="submit"
-                value="Create new Session"
-            />
-        </form>
+        <div class="hero has-text-centered is-light">
+            <h1 class="is-size-1 is-size-3-mobile">
+                Wellcome to Planning Poker
+            </h1>
+        </div>
+
+        <div class="section">
+            <div class="column is-10 is-offset-1">
+                <div class="notification is-size-5-desktop">
+                    <p class="is-hidden-mobile">
+                        Planning Poker is a webapp to help you organise your
+                        User Story estimation meetings. Open a new session and
+                        invite your colleagues to join! All members can vote
+                        anonymously, nobody knows the results before everybody
+                        voted and the outcome is presented in a nice graph. The
+                        results are stored in the clients themselves, which
+                        communicate with each other via web sockets. None of
+                        your data are stored on the server. So once all members
+                        of the session close their windows all results are gone.
+                    </p>
+                    <p class="is-hidden-tablet is-size-6-mobile">
+                        Planning Poker is a webapp to help you organise your
+                        User Story estimation meetings. Open a new session and
+                        invite your colleagues to join! None of your data are
+                        stored on the server. So once all members of the session
+                        close their windows all results are gone.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <form (submit)="CreateNewSession()">
+                <div class="block">
+                    <div class="field has-addons has-addons-centered">
+                        <p class="control">
+                            <input
+                                class="input"
+                                name="sessionName"
+                                placeholder="Session Name"
+                                [(ngModel)]="sessionName"
+                                required
+                            />
+                        </p>
+                        <p class="control">
+                            <input
+                                class="button is-info"
+                                [disabled]="!this.sessionName"
+                                type="submit"
+                                value="Create new Session"
+                            />
+                        </p>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="footer is-fixed-bottom">
+            <div class="level is-mobile">
+                <div class="level-left">
+                    <a
+                        href="https://github.com/TimHolzherr/planning-poker"
+                        class="level-item"
+                    >
+                        <span class="icon is-medium">
+                            <i class="fab fa-github"></i>
+                        </span>
+                        <span>GitHub</span>
+                    </a>
+                </div>
+                <div class="level-right">
+                    <a
+                        href="https://www.linkedin.com/in/tim-holzherr-63442ab4/"
+                        class="level-item"
+                    >
+                        <span>Tim Holzherr</span>
+                        <span class="icon is-medium">
+                            <i class="fab fa-linkedin"></i>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
     `,
     styles: []
 })
