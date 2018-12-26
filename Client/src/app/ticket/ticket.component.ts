929efc1d9974df5b4c6e46bd59a4a41facf49286
TimHolzherr <tim_holzherr@bluewin.ch>
2018-12-26 17:45:30 +0100

    Extract templates into there own files

--- Client/src/app/ticket/ticket.component.ts
+++ Client/src/app/ticket/ticket.component.ts
@@ -4,39 +4,7 @@ import { BackendService } from "../backend.service";
 
 @Component({
     selector: "app-ticket",
-    template: `
-        <ng-container *ngIf="!ticket.hasVoted && !ticket.voteFinished">
-            <div class="title is-4 has-text-centered">
-                <p>
-                    Please vote for ticket: <strong> {{ ticket.name }}</strong>
-                </p>
-            </div>
-            <div class="columns is-multiline is-mobile">
-                <app-vote-button
-                    *ngFor="let cp of complexityPoints"
-                    [complexityPoints]="cp"
-                    (vote)="vote($event)"
-                >
-                </app-vote-button>
-                <app-vote-button (vote)="vote($event)"> </app-vote-button>
-            </div>
-        </ng-container>
-        <div *ngIf="ticket.hasVoted && !ticket.voteFinished">
-            <p class="has-text-centered">
-                You voted <strong>{{ ticket.yourVote }}</strong
-                >, waiting for
-                <strong> {{ numberOfUsers - this.ticket.votes.length }}</strong>
-                other vote{{
-                    numberOfUsers - this.ticket.votes.length > 1 ? "s" : ""
-                }}.
-            </p>
-        </div>
-        <div class="has-text-centered" *ngIf="ticket.voteFinished">
-            <div class="title is-4">Results for ticket {{ ticket.name }}</div>
-            <p>All votes: {{ ticket.votes.join(", ") }}</p>
-            <p>Average: {{ ticket.getAverageVote() }}</p>
-        </div>
-    `
+    templateUrl: "./ticket.component.html"
 })
 export class TicketComponent implements OnInit {
     constructor(private backendService: BackendService) {}
