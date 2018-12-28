2e3a7d65ad67d3f18d1b3b1d34e3ad289c89dd72
TimHolzherr <tim_holzherr@bluewin.ch>
2018-12-28 12:37:47 +0100

    Implement fake server side rendering

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,13 +1,21 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .then(hideServerSideRenderedContent)
+    .catch(err => console.error(err));
 
+function hideServerSideRenderedContent(): void {
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".server-side-rendered { display: none }";
+    document.body.appendChild(css);
+}
