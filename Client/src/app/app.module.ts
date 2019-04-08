6a335663994c9c855e0aaf84c8b44b9d9f3866fb
TimHolzherr <tim_holzherr@bluewin.ch>
2019-04-08 20:38:30 +0200

    Add animation for ticket change

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule, Title } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 
@@ -27,6 +28,7 @@ import { ShareComponent } from "./session/share/share.component";
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         AppRoutingModule,
         FormsModule,
         ServiceWorkerModule.register("ngsw-worker.js", {
--- Client/src/app/ticket/ticket.component.ts
+++ Client/src/app/ticket/ticket.component.ts
@@ -1,10 +1,36 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { TicketModel } from "../models/ticket.model";
 import { BackendService } from "../backend.service";
+import {
+    trigger,
+    style,
+    animate,
+    transition,
+    keyframes,
+} from "@angular/animations";
 
 @Component({
     selector: "app-ticket",
     templateUrl: "./ticket.component.html",
+    animations: [
+        trigger("fadeIn", [
+            transition("* => *", [
+                animate(
+                    "1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000)",
+                    keyframes([
+                        style({
+                            opacity: "0",
+                            offset: 0,
+                        }),
+                        style({
+                            opacity: "1",
+                            offset: 1,
+                        }),
+                    ])
+                ),
+            ]),
+        ]),
+    ],
 })
 export class TicketComponent implements OnInit {
     constructor(private backendService: BackendService) {}
@@ -13,11 +39,20 @@ export class TicketComponent implements OnInit {
 
     ngOnInit() {}
 
+    private _ticket: TicketModel;
     @Input()
-    public ticket: TicketModel;
+    public get ticket(): TicketModel {
+        return this._ticket;
+    }
+    public set ticket(value: TicketModel) {
+        this._ticket = value;
+        this.animationChanged = !this.animationChanged;
+    }
 
     @Input()
     public numberOfUsers: number;
+
+    public animationChanged = false;
 
     get remainingVotes(): number {
         return this.numberOfUsers - this.ticket.votes.length;
